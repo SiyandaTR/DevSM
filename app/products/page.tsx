401bc7b27e0bca927bae8183bda3014eecae49e3
Siyanda Mkhize <siyandam097@gmail.com>
2023-08-23 11:02:42 +0200

    padding

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,7 +9,7 @@ export default function Example() {
 	return (
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
-            <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+            <h1 className="py-20 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				S MKHIZE
 			</h1>
 			
