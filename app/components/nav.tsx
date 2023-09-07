880752f374f5aeafb5097da0a712f652034a52ba
Siyanda Mkhize <siyandam097@gmail.com>
2023-09-07 07:33:59 +0200

    navigation

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,6 +2,7 @@
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 export const Navigation: React.FC = () => {
 	const ref = useRef<HTMLElement>(null);
@@ -19,24 +20,26 @@ export const Navigation: React.FC = () => {
 
 	return (
 		<header ref={ref}>
-			<div
+			<motion.div
 				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
 					isIntersecting
 						? "bg-zinc-900/0 border-transparent"
 						: "bg-zinc-900/500  border-zinc-800 "
 				}`}
+				initial={{ y: -100, opacity: 0 }}
+        		animate={{ y: 0, opacity: 1 }}
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
 						<Link
 							href="/projects"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
+							className="duration-200 text-green-950 hover:text-green-700 dark:text-zinc-400 dark:hover:text-zinc-100"
 						>
 							Projects
 						</Link>
 						<Link
 							href="/contact"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
+							className="duration-200 text-green-950 hover:text-green-700 dark:text-zinc-400 dark:hover:text-zinc-100"
 						>
 							Contact
 						</Link>
@@ -44,7 +47,7 @@ export const Navigation: React.FC = () => {
 						
 						<Link
 							href="/products"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
+							className="duration-200 text-green-950 hover:text-green-700 dark:text-zinc-400 dark:hover:text-zinc-100"
 						>
 							Products
 						</Link>
@@ -52,12 +55,12 @@ export const Navigation: React.FC = () => {
 
 					<Link
 						href="/"
-						className="duration-200 text-zinc-300 hover:text-zinc-100"
+						className="duration-200 text-green-950 hover:text-green-700 dark:text-zinc-300 dark:hover:text-zinc-100"
 					>
 						<ArrowLeft className="w-6 h-6 " />
 					</Link>
 				</div>
-			</div>
+			</motion.div>
 		</header>
 	);
 };
