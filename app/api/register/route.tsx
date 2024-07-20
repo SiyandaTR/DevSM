17a0fe6383f12a53e5e7219e531637ce6be47b1b
Siyanda Mkhize <siyandam097@gmail.com>
2024-07-20 09:47:18 +0200

    Branche control

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -1,35 +1,35 @@
+// // import { prisma } from '@/lib/prisma';
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
 
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+// const prisma = new PrismaClient();  
 
-const prisma = new PrismaClient();
+// export async function POST(request: any) {
+//     const body = await request.json();
+//     const {name, email, password} = body;
+//     console.log(body);
 
-export async function POST(request: any) {
-    const body = await request.json();
-    const {name, email, password} = body;
-    console.log(body);
+//     if (!name || !email ||!password) {
+//         return new NextResponse("Missing name, email, password", {status: 400});
+//     }
 
-    if (!name || !email ||!password) {
-        return new NextResponse("Missing name, email, password", {status: 400});
-    }
+//     const exist = await prisma.user.findUnique({
+//         where: {
+//             email: email
+//         }
+//     });
+//     if (exist) {
+//         return new NextResponse("User already exists", {status: 400})
+//     }
 
-    const exist = await prisma.user.findUnique({
-        where: {
-            email: email
-        }
-    });
-    if (exist) {
-        return new NextResponse("User already exists", {status: 400})
-    }
-
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password
-        }
-    });
+//     // const hashedPassword = await bcrypt.hash(password, 10);
+//     const user = await prisma.user.create({
+//         data: {
+//             name,
+//             email,
+//             password
+//         }
+//     });
      
-    return NextResponse.json(user)
-}
+//     return NextResponse.json(user)
+// }
