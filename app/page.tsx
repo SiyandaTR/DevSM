b81bed7a4e7d176d1cb3e96bc7f343757117e808
Siyanda Mkhize <siyandam097@gmail.com>
2024-07-24 13:26:50 +0200

    Text Gradient

--- app/page.tsx
+++ app/page.tsx
@@ -34,12 +34,13 @@ export default function Home() {
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={3000}
-				ease={10}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				SIYANDA FRONT
-			</h1>
-
+			<div>
+				<h1 className="text-gradient z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+					SIYANDA FRONT
+				</h1>
+			</div>
+			
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-700 dark:text-zinc-100 ">
