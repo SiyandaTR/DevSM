2bf4bac890c0254d2fb47eb3d247aee9cb88af68
Siyanda Mkhize <siyandam097@gmail.com>
2024-07-31 11:27:22 +0200

    Front-end update

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-700 dark:text-zinc-100 ">
-					My name is Siyanda Mkhize From Durban KwaZulu, a Web developer 
+					My name is Siyanda Mgazi From Durban KwaZulu, a Web developer 
 					Using the latest technology{" "}
 					<Link
 						target="_blank"
