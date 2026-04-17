8295e1fb574f8a70a7573034b1fb3b38febab9db
Siyanda Corey Mkhize <75018304+SiyandaTR@users.noreply.github.com>
2026-04-17 05:38:05 +0000

    deploy

--- components/header.tsx
+++ components/header.tsx
@@ -64,7 +64,7 @@ export default function Header({ activeSection, onSectionChange }: HeaderProps) 
       </nav>
 
       {/* Button Group with Arrow */}
-      <div id="gooey-btn" className="relative flex items-center group" style={{ filter: "url(#gooey-filter)" }}>
+      {/* <div id="gooey-btn" className="relative flex items-center group" style={{ filter: "url(#gooey-filter)" }}>
         
           <button className="absolute right-0 px-2.5 py-2 rounded-full bg-white text-black font-normal text-xs transition-all duration-300 hover:bg-white/90 cursor-pointer h-8 flex items-center justify-center -translate-x-10 group-hover:-translate-x-27 z-0">
             <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -74,7 +74,7 @@ export default function Header({ activeSection, onSectionChange }: HeaderProps) 
           <button className="px-6 py-2 rounded-full bg-white text-black font-normal text-xs transition-all duration-300 hover:bg-white/90 cursor-pointer h-8 flex items-center z-10">
             Contact Me
           </button>
-        </div>
+        </div> */}
 
         <div id="gooey-btn" className="relative flex items-center group" style={{ filter: "url(#gooey-filter)" }}>
         <DirectMessagePopover>
